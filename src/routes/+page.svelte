d7c3aff087d0ae822f71a7c501cf2406372210e9
honehone <honehone.41games@gmail.com>
2025-03-10 12:14:51 +0900

    added css library

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Kenja</h1>
-<p>Visit <a href="https://https://github.com/honehone12/kenja-web"> github</a></p>
+<h1 class="text-xl">Kenja</h1>
+<p>
+    Visit 
+    <a 
+        href="https://https://github.com/honehone12/kenja-web"
+        class="link link-primary" 
+    > 
+        github
+    </a>
+</p>
