387cd35961126dc3c35d977f76635bd6b37f73a8
honehone <honehone.41games@gmail.com>
2025-03-12 18:03:23 +0900

    changed grpc library

--- gen_grpc.sh
+++ gen_grpc.sh
@@ -3,11 +3,8 @@
 set -e
 
 ./node_modules/.bin/grpc_tools_node_protoc \
-    --js_out=import_style=commonjs,binary:src/lib/server/grpc \
-    --grpc_out=grpc_js:src/lib/server/grpc \
+    --plugin=protoc-gen-ts_proto=./node_modules/.bin/protoc-gen-ts_proto \
+    --ts_proto_out=./src/lib/server/grpc \
+    --ts_proto_opt=outputServices=nice-grpc,outputServices=generic-definitions,useExactTypes=false \
     --proto_path=../kenja-proto \
     ../kenja-proto/anime_search.proto 
-
-for file in src/lib/server/grpc/*.js; do
-    mv "$file" "${file%.js}.cjs"
-done
