61232207e23916f0c2e601e2dd9efbe34f7e2faa
honehone <honehone.41games@gmail.com>
2025-03-13 16:34:53 +0900

    fixed search input css

--- src/lib/components/SearchInput.svelte
+++ src/lib/components/SearchInput.svelte
@@ -1,39 +1,37 @@
-<div class="w-screen">
-    <form method="get" action="search">
-        <div class="mb-5 ml-5">
-            <label for="rating">
-                <span class="text-xl">Search For Hentai ??</span>
-                <input 
-                    type="checkbox" 
-                    name="rating"
-                    value="hentai"
-                    id="rating"
-                    class="toggle toggle-secondary ml-5" 
-                />
-            </label>
-        </div>
+<form method="get" action="search">
+    <div class="mb-5 ml-5">
+        <label for="rating">
+            <span class="text-xl">Search For Hentai ??</span>
+            <input 
+                type="checkbox" 
+                name="rating"
+                value="hentai"
+                id="rating"
+                class="toggle toggle-secondary ml-5" 
+            />
+        </label>
+    </div>
 
-        <div class="join w-full">
-            <label 
-                for="keyword" 
-                class="join-item input input-xl w-full max-w-3xl grow"
-            >
-                <svg class="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g stroke-linejoin="round" stroke-linecap="round" stroke-width="2.5" fill="none" stroke="currentColor"><circle cx="11" cy="11" r="8"></circle><path d="m21 21-4.3-4.3"></path></g></svg>
-                <input 
-                    type="text"
-                    name="keyword" 
-                    required
-                    maxlength="50"
-                    placeholder="Enter Keywords"
-                    id="keyword"
-                > 
-            </label>
-            <button
-                type="submit" 
-                class="join-item btn btn-xl mr-10"
-            >
-                Search
-            </button>
-        </div>
-    </form>
-</div>
+    <div class="join w-screen max-w-3xl">
+        <label 
+            for="keyword" 
+            class="join-item input input-xl grow"
+        >
+            <svg class="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g stroke-linejoin="round" stroke-linecap="round" stroke-width="2.5" fill="none" stroke="currentColor"><circle cx="11" cy="11" r="8"></circle><path d="m21 21-4.3-4.3"></path></g></svg>
+            <input 
+                type="text"
+                name="keyword" 
+                required
+                maxlength="50"
+                placeholder="Enter Keywords"
+                id="keyword"
+            > 
+        </label>
+        <button
+            type="submit" 
+            class="join-item btn btn-xl mr-10"
+        >
+            Search
+        </button>
+    </div>
+</form>
