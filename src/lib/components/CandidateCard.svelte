63b68800a7e03a4e07ce18b5e203d81ff811c993
honehone <honehone.41games@gmail.com>
2025-03-13 21:38:40 +0900

    added badges

--- src/lib/components/CandidateCard.svelte
+++ src/lib/components/CandidateCard.svelte
@@ -1,4 +1,6 @@
 <script>
+    import {page} from "$app/state";
+    
     /**
      * @type {{
      *  url: string;
@@ -18,30 +20,30 @@
         nameEnglish,
         nameJapanese
     } = $props();
+    let titleColor = page.url.searchParams.get('rating') 
+        ? 'secondary' : 'accent';
 </script>
 
 <div class="card card-xl min-w-full max-w-200 bg-base-200 card-border">
     <div class="card-body">
         {#if parent}
-            <span class="badge badge-accent">
+            <span class="badge badge-outline badge-{titleColor}">
                 <h3 class="text-base">Character キャラクター</h3>
             </span>    
-            <div class="card info">
-                <h3 class="text-sm">
-                    {parent.name} {parent.nameJapanese ? parent.nameJapanese : ''}
-                </h3>
-            </div>
+            <h3 class="text-sm">
+                {parent.name}/{parent.nameJapanese ? parent.nameJapanese : ''}
+            </h3>
         {:else}
-            <span class="badge badge-accent">
+            <span class="badge badge-outline badge-{titleColor}">
                 <h3 class="text-base">Anime アニメ</h3>
             </span>
         {/if}
         <h1 class="card-title">{name}</h1>
-        {#if nameJapanese}
-            <h1 class="card-title text-info">{nameJapanese}</h1>
-        {/if}
         {#if nameEnglish}
-            <h3 class="text-lg">{nameEnglish}</h3>
+            <h3 class="text-lg">({nameEnglish})</h3>
+        {/if}
+        {#if nameJapanese}
+            <h1 class="card-title text-{titleColor}">{nameJapanese}</h1>
         {/if}
         <a href="{url}" class="link link-primary">MyAnimeList</a>
     </div>
--- src/lib/components/SearchInput.svelte
+++ src/lib/components/SearchInput.svelte
@@ -1,37 +1,62 @@
-<form method="get" action="search">
-    <div class="mb-5 ml-5">
-        <label for="rating">
-            <span class="text-xl">Search For Hentai ??</span>
-            <input 
-                type="checkbox" 
-                name="rating"
-                value="hentai"
-                id="rating"
-                class="toggle toggle-secondary ml-5" 
-            />
-        </label>
-    </div>
+<script>
+    let isHentai = $state(false);
+    let toggleText = $derived(isHentai ? 'Hentai' : 'Normal');
+    let toggleColor = $derived(isHentai ? 'secondary' : 'neutral');
+</script>
 
-    <div class="join w-screen max-w-3xl">
-        <label 
-            for="keyword" 
-            class="join-item input input-xl grow"
-        >
-            <svg class="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g stroke-linejoin="round" stroke-linecap="round" stroke-width="2.5" fill="none" stroke="currentColor"><circle cx="11" cy="11" r="8"></circle><path d="m21 21-4.3-4.3"></path></g></svg>
-            <input 
-                type="text"
-                name="keyword" 
-                required
-                maxlength="50"
-                placeholder="Enter Keywords"
-                id="keyword"
-            > 
-        </label>
-        <button
-            type="submit" 
-            class="join-item btn btn-xl"
-        >
-            Search
-        </button>
+<div>
+    
+    <form method="get" action="search">
+        <div class="join w-screen max-w-3xl">
+            <label 
+                for="keyword" 
+                class="join-item input input-xl grow"
+            >
+                <svg class="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g stroke-linejoin="round" stroke-linecap="round" stroke-width="2.5" fill="none" stroke="currentColor"><circle cx="11" cy="11" r="8"></circle><path d="m21 21-4.3-4.3"></path></g></svg>
+                <input 
+                    type="text"
+                    name="keyword" 
+                    required
+                    maxlength="50"
+                    placeholder="Enter Keywords"
+                    id="keyword"
+                > 
+            </label>
+            <button
+                type="submit" 
+                class="join-item btn btn-xl"
+            >
+                Search
+            </button>
+        </div>
+        <div class="mt-5 ml-5">
+            <label for="rating">
+                <span class="font-mono text-lg text-{toggleColor}">
+                    {toggleText}
+                </span>
+                <input 
+                    type="checkbox" 
+                    name="rating"
+                    value="hentai"
+                    id="rating"
+                    class="toggle toggle-secondary ml-5"
+                    bind:checked={isHentai}
+                />
+            </label>
+        </div>
+    </form>
+    <div class="mt-5 ml-5">
+        {#if isHentai}
+            <div class="badge badge-dash badge-secondary text-sm">
+                I am over 18 years old.
+            </div>
+            <div class="badge badge-dash badge-secondary text-sm">
+                私は１８歳以上です。
+            </div>
+        {:else}
+            <div class="badge badge-dash badge-info text-sm">
+                All Ages
+            </div>
+        {/if}
     </div>
-</form>
+</div>
