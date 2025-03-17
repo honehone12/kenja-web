9bd4602748251224d2fb1782482d87806fbfc6ae
honehone <honehone.41games@gmail.com>
2025-03-17 12:07:57 +0900

    added logo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,17 @@
 </script>
 
 <div class="hero h-screen">
-    <div class="hero-content">
+    
+    <div class="hero-content flex-col w-screen max-w-3xl">
+        <figure class="mb-10">
+            <img
+                src="KenjaMedium.png"
+                alt="Kenja logo with english and japanese kanji"
+                class="rounded-xl max-w-xs" 
+            />
+        </figure>
+        <p>Kenja. A Useless Anime Search Engine.</p>
+        <p class="mb-10">賢者。役立たずなアニメのサーチエンジン。</p>
         <SearchInput/>
     </div>
 </div>
