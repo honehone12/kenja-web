93123fcca1f2e3167d7aa2c31768a87c51c4e762
honehone <honehone.41games@gmail.com>
2025-03-19 18:17:26 +0900

    fixed navbar

--- src/lib/components/layout/Navbar.svelte
+++ src/lib/components/layout/Navbar.svelte
@@ -4,9 +4,9 @@
 			<div tabindex="0" role="button" class="btn btn-ghost">
 				<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6 h16 M4 12 h16 m-16 6 h16"/></svg>
 			</div>
-			<ul class="menu dropdown-content bg-base-200 rounded-box z-1 w-52 mt-3 p-2">
-				<li><a tabindex="0" href="/">Home</a></li>
-				<li><a tabindex="0" href="/about">About</a></li>
+			<ul class="menu menu-lg dropdown-content bg-base-300 rounded-box z-1 w-52 mt-4 p-3">
+				<li class="my-2"><a tabindex="0" href="/">Home</a></li>
+				<li class="my-2"><a tabindex="0" href="/about">About</a></li>
 			</ul>
 		</div>
 	</div>
