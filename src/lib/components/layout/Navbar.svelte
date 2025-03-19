cfba8d79e6126395d38aaddf97a94617cf601e78
honehone <honehone.41games@gmail.com>
2025-03-19 18:28:17 +0900

    fixed menu size

--- src/lib/components/layout/Navbar.svelte
+++ src/lib/components/layout/Navbar.svelte
@@ -2,7 +2,7 @@
 	<div class="navbar-start">
 		<div class="dropdown md:hidden">
 			<div tabindex="0" role="button" class="btn btn-ghost">
-				<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6 h16 M4 12 h16 m-16 6 h16"/></svg>
+				<svg xmlns="http://www.w3.org/2000/svg" class="h-7 w-7" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6 h16 M4 12 h16 m-16 6 h16"/></svg>
 			</div>
 			<ul class="menu menu-lg dropdown-content bg-base-300 rounded-box z-1 w-52 mt-4 p-3">
 				<li class="my-2"><a tabindex="0" href="/">Home</a></li>
@@ -11,7 +11,7 @@
 		</div>
 	</div>
 	<div class="navbar-center hidden md:flex">
-		<ul class="menu menu-horizontal px-1">
+		<ul class="menu menu-lg menu-horizontal px-1">
 			<li><a tabindex="0" href="/">Home</a></li>
 			<li><a tabindex="0" href="/about">Abount</a></li>
 		</ul>
