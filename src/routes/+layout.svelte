6a06b33b1ad51b080d6f8e9f18de36ee67f9f447
honehone <honehone.41games@gmail.com>
2025-04-22 15:41:12 +0900

    fixed layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script>
 	import Footer from "$lib/components/layout/Footer.svelte";
-    import Navbar from "$lib/components/layout/Navbar.svelte";
+	import Navbar from "$lib/components/layout/Navbar.svelte";
 	import "../app.css";
 	
 	let { children } = $props();
